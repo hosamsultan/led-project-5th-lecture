a072e49f02a0a6f4a29751ddc39ed5e9640b34e1
hosamsultan <54276748+hosamsultan@users.noreply.github.com>
2020-09-25 14:12:51 +0200

    led project 5th lecture

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 24, 2020
+ *      Author: Hossam-PC
+ */
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "RCC_interface.h"
+
+#define PORTA_CRL *((u32*)0x40010800)
+#define PORTA_ODR *((u32*)0x4001080C)
+void main(void)
+{
+	/*initialize clock system*/
+	RCC_voidInitSysClock();
+	/*Enable peripheral 2 on APB2 bus (GPIOA)*/
+	RCC_voidEnableClock(RCC_APB2,2);
+	/*configure */
+	PORTA_CRL = 0x00000002;
+	/*set A0 output*/
+	PORTA_ODR  = 0x00000001;
+	while(1)
+	{
+
+	}
+}
